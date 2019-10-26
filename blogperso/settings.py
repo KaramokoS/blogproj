054d312c4379dcbcd0f28dac91037cd72a7de3b7
Kara SAMASSA <samss.kara@gmail.com>
2019-10-26 16:37:03 +0200

    fixing staticfiles path

--- blogperso/settings.py
+++ blogperso/settings.py
@@ -17,7 +17,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR,'dakuoblog/templates/dakuoblog')
 STATIC_DIRS = os.path.join(BASE_DIR,'dakuoblog/static')
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
